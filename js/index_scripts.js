e36e99506881a7b519e8f1533918c54fd1ceeb1d
jiaminheLabPC <jiaminhe@mit.edu>
2017-03-14 10:50:57 -0400

    test time

--- js/index_scripts.js
+++ js/index_scripts.js
@@ -25,6 +25,10 @@ window.onload = function() {
     'Hey there 👋. How are you doing? I\'m <a target="_blank" href="http://jiaminhe.com" >Jiamin He. </a><br><a target="_blank" href=" ">Interesting facts of me</a> ',
     'I do...(work)(link to github.) I\'m currently ...👇<br><a target="_blank" href="https://twitter.com/juliangarnier">twitter.com/juliangarnier</a><br><a target="_blank" href="https://codepen.io/juliangarnier">codepen.io/juliangarnier</a><br><a target="_blank" href="https://github.com/juliangarnier">github.com/juliangarnier</a><br> You can contact me at <a href="mailto:hejiamin1995@gmail.com">hejiamin1995@gmail.com</a>',
     'I like...(life,hobbies,social network)<br>links<br><a target="_blank" href="http://jiaminhe.com/about">about me</a><br><a target="_blank" href="http://jiaminhe.com/blog">blog</a>',
+    'Hey there 👋. How are you doing? I\'m <a target="_blank" href="http://jiaminhe.com" >Jiamin He. </a><br><a target="_blank" href=" ">Interesting facts of me</a> ',
+    'I do...(work)(link to github.) I\'m currently ...👇<br><a target="_blank" href="https://twitter.com/juliangarnier">twitter.com/juliangarnier</a><br><a target="_blank" href="https://codepen.io/juliangarnier">codepen.io/juliangarnier</a><br><a target="_blank" href="https://github.com/juliangarnier">github.com/juliangarnier</a><br> You can contact me at <a href="mailto:hejiamin1995@gmail.com">hejiamin1995@gmail.com</a>',
+    'I like...(life,hobbies,social network)<br>links<br><a target="_blank" href="http://jiaminhe.com/about">about me</a><br><a target="_blank" href="http://jiaminhe.com/blog">blog</a>',
+
 //     '',
     getCurrentTime(),
   ]
